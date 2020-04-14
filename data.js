1626eb468b0cab54ebceb6d01add862ad8b1acbd
haif.yao <haif.yao@gmail.com>
2020-04-14 21:02:24 +0800

    version one

---
+++ data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    name: 'A surgical mask',
+    describe: 'Elastic Ear-loop style(17.5*9.5cm)',
+    standard: 'YY/T 0969-2013',
+    license: 'CE DOC',
+    img: require('@/assets/products/product_1@2x.png'),
+  },
+  {
+    name: 'KN95 N95 FFP2  Protective Mask',
+    describe: 'Elastic Ear-loop style',
+    standard: 'CN: T/CTCA 7-2019',
+    license: 'CE DOC',
+    img: require('@/assets/products/product_2@2x.png'),
+  },
+  {
+    name: 'VINYL GLOVES',
+    describe: 'S/M/L/XL',
+    standard: 'European EN455',
+    license: 'CE certification',
+    img: require('@/assets/products/product_3@2x.png'),
+  },
+  {
+    name: 'Protective Overall',
+    describe: 'Medical Grade Protection purpose Brand: BH Model: BH800',
+    standard: 'EN14126 EN14605/466  Coverall Type3',
+    license: 'CE certification',
+    img: require('@/assets/products/product_4@2x.png'),
+  },
+  {
+    name: 'SAFETY GOGGLES',
+    describe: '17cm*8cm*7.5cm',
+    standard: 'European EN 166',
+    license: 'CE certification',
+    img: require('@/assets/products/product_5@2x.png'),
+  },
+  {
+    name: 'LyFamily 75% Alcohol Leave-on Hand Sanitizer Gel',
+    describe: '60ml / 250ml / 500ml',
+    standard: 'EN1499：2013',
+    license: 'CE certification',
+    img: require('@/assets/products/product_6@2x.png'),
+  },
+];
